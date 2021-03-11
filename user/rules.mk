3f3a95243382244357b2c9b755f37a112fedad4c
James Simpson <edenofest@gmail.com>
2021-03-11 14:29:19 -0500

    feat: Add common build flags to user rules.mk

--- user/rules.mk
+++ user/rules.mk
@@ -1,6 +1,12 @@
 src += eden.c \
 	   process_records.c
 
+# Common flags
+LTO_ENABLE      = yes # Optimize at link time for resulting smaller files
+EXTRAKEY_ENABLE = yes # Audio controls and system controls
+NKRO_ENABLE     = yes # NKey Rollover
+COMMAND_ENABLE  = yes # Commands for debug and configuration
+
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
     SRC += tap_dances.c
 endif
