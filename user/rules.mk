6b35503fc11daabcbd282ed165d2a933bd54346d
EdenEast <edenofest@gmail.com>
2023-05-07 00:01:43 -0400

    chore: update qmk firmware

--- user/rules.mk
+++ user/rules.mk
@@ -17,10 +17,10 @@ COMBO_ENABLE       = yes # Combo keys together
 LTO_ENABLE         = yes # Optimize at link time for resulting smaller files
 EXTRAKEY_ENABLE    = yes # Audio controls and system controls
 NKRO_ENABLE        = yes # NKey Rollover
-TAP_DANCE_ENABLE   = yes # Used for tmux prefix key
 STENO_ENABLE       = yes # Enable steno
 STENO_PROTOCOL     = geminipr # Better protocol for steno
 VIRTSER_ENABLE     = yes # Required for steno
+# TAP_DANCE_ENABLE   = yes # Used for tmux prefix key
 
 # Add LTO compile flag
 # https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
