58ddd2c96c5f7bd993f919c9d34ecbdc30cfdf57
EdenEast <edenofest@gmail.com>
2023-11-15 10:33:16 -0500

    feat(tofu): Change shift to be one shots

--- qmk/keyboards/dztech/dz60v2/keymaps/edeneast/keymap.c
+++ qmk/keyboards/dztech/dz60v2/keymaps/edeneast/keymap.c
@@ -7,6 +7,8 @@
 #define MOR MO(_RAISE)
 #define TGG TG(_GAME)
 #define MIC MUTE_MIC
+#define OSSL OSM(MOD_LSFT)
+#define OSSR OSM(MOD_RSFT)
 
 enum keymap_keycodes {
   TG_SETT = NEW_SAFE_RANGE, // Toggle settings
@@ -33,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     CTR_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    OSSL,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSSR,
     KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  MOR,     KC_SPC,           KC_LALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
     // KC_LCTL, TD_LGUI, TD_LALT,          KC_BSPC, MO(_RAISE),   KC_SPC,           TD_LALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
   ),
@@ -46,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------+
  * |         |     |     |     |     |     |     |     |     |     |     |     |             |
  * |------------------------------^-----------------^----------------------------------------+
- * |           |     |     |     |     |     |     |     |     |     |     |                 |
+ * |   Shift   |     |     |     |     |     |     |     |     |     |     |      Shift      |
  * |-----------------------------------------------------------------------------------------+
- * |      |       |       |   Space   |      |                |     |      |     |     |     |
+ * | Ctrl | Super |  Alt  |   Space   |      |     Space      |     |      |     |     |     |
  * `-----------------------------------------------------------------------------------------'
  */
 
@@ -56,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______,          KC_SPC,  _______, _______,          _______, _______, _______, _______, _______
+    KC_LSFT,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  _______, KC_SPC,          _______, _______, _______, _______, _______
   ),
 
 
