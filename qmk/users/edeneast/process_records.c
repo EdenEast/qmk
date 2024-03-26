452488b485e05c6561c93abd55465ea653c69958
EdenEast <edenofest@gmail.com>
2024-03-25 23:29:14 -0400

    fix(arc): change symbol double to single quotes

--- qmk/users/edeneast/process_records.c
+++ qmk/users/edeneast/process_records.c
@@ -166,11 +166,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         clear_oneshot_mods(); // Temporarily disable mods.
         unregister_mods(MOD_MASK_CSAG);
         if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
-          SEND_STRING("\"\"");
+          SEND_STRING("''");
         } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
           SEND_STRING("``");
         } else {
-          SEND_STRING("''");
+          SEND_STRING("\"\"");
         }
         tap_code(KC_LEFT);
         register_mods(mods);
