533c350c54cf5753cc6a0e200e3597014c39e1c7
EdenEast <edenofest@gmail.com>
2024-04-08 14:04:38 -0400

    fix(user): set defaults for user rules

--- qmk/users/edeneast/rules.mk
+++ qmk/users/edeneast/rules.mk
@@ -1,20 +1,22 @@
 # Common flags
-AUDIO_ENABLE          = no  # Use the audio system
-BOOTMAGIC_ENABLE      = no  # Disable to allow keys during system boot
-COMMAND_ENABLE        = no  # Commands for debug and configuration
-CONSOLE_ENABLE        = no  # Console for debugging
-GRAVE_ESC_ENABLE      = no  # Dont use it, save space
-MIDI_ENABLE           = no  # Dont use it, save space
-RGBLIGHT_ENABLE       = no  # No rgb
-SPACE_CADET_ENABLE    = no  # Dont use it, save space
+AUDIO_ENABLE         ?= no  # Use the audio system
+BOOTMAGIC_ENABLE     ?= no  # Disable to allow keys during system boot
+COMMAND_ENABLE       ?= no  # Commands for debug and configuration
+CONSOLE_ENABLE       ?= no  # Console for debugging
+GRAVE_ESC_ENABLE     ?= no  # Dont use it, save space
+MIDI_ENABLE          ?= no  # Dont use it, save space
+RGBLIGHT_ENABLE      ?= no  # No rgb
+SPACE_CADET_ENABLE   ?= no  # Dont use it, save space
 
-COMBO_ENABLE          = yes # Combo keys together
-LTO_ENABLE            = yes # Optimize at link time for resulting smaller files
-EXTRAKEY_ENABLE       = yes # Audio controls and system controls
-OS_DETECTION_ENABLE  ?= yes # Enable os detection
-DEFERRED_EXEC_ENABLE ?= yes # Enable defer execution for os detection startup
+COMBO_ENABLE         ?= yes # Combo keys together
 TAP_DANCE_ENABLE     ?= no  # Used for tmux prefix key
-LEADER_ENABLE     ?= no  # Enable leader keys
+LEADER_ENABLE        ?= no  # Enable leader keys
+
+# Required features
+DEFERRED_EXEC_ENABLE = yes # Enable defer execution for os detection startup
+EXTRAKEY_ENABLE      = yes # Audio controls and system controls
+LTO_ENABLE           = yes # Optimize at link time for resulting smaller files
+OS_DETECTION_ENABLE  = yes # Enable os detection
 
 ifeq ($(strip $(STENO_ENABLE)), yes)
 	STENO_PROTOCOL     = geminipr # Better protocol for steno
@@ -24,16 +26,18 @@ endif
 # Feature enables
 SENTENCE_CASE_ENABLE = yes
 
-# Add LTO compile flag
-# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
-EXTRAFLAGS += -flto
-
 SRC += edeneast.c
 SRC += process_records.c
 SRC += smart_case.c
 SRC += os_detect.c
 SRC += callbacks.c
 SRC += features/layer_lock.c
+
+ifeq ($(strip $(DEBUG_ENABLE)), yes)
+	COMMAND_ENABLE = yes  # Commands for debug and configuration
+	CONSOLE_ENABLE = yes  # Console for debugging
+	OPT_DEFS += -DDEBUG_ENABLE
+endif
 
 ifeq ($(strip $(COMBO_ENABLE)), yes)
 	INTROSPECTION_KEYMAP_C = combos.c
@@ -63,5 +67,5 @@ endif
 
 # Enable this for boards that dont have a shift key (macro pads)
 ifeq ($(strip $(FLASH_BOOTLOADER)), yes)
-    OPT_DEFS += -DFLASH_BOOTLOADER
+	OPT_DEFS += -DFLASH_BOOTLOADER
 endif
