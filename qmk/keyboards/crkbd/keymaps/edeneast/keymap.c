ddef5bdbb2060505149d4447104a99e608acbd0f
EdenEast <edenofest@gmail.com>
2024-11-03 11:32:20 -0500

    fix(crkbd): disable OLED and remove tap dance

--- qmk/keyboards/crkbd/keymaps/edeneast/keymap.c
+++ qmk/keyboards/crkbd/keymaps/edeneast/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     __STENO_L3__________________________________________,                            __STENO_R3__________________________________________,
                                         _______, STN_A,   STN_O,            STN_E,   STN_U,   _______
   ),
-  #endif
+#endif
 
 };
 
--- qmk/keyboards/crkbd/keymaps/edeneast/rules.mk
+++ qmk/keyboards/crkbd/keymaps/edeneast/rules.mk
@@ -1,5 +1,5 @@
-RGBLIGHT_ENABLE      = no  # No rgb
-
-OLED_ENABLE          = yes
-OLED_DRIVER          = ssd1306
-ACHORDION_ENABLE     = yes
+OLED_ENABLE        = no
+OLED_DRIVER        = ssd1306
+ACHORDION_ENABLE   = yes
+MOUSEKEY_ENABLE    = no # Force no mouse keys to save space
+BOOTMAGIC_ENABLE   = no # Disable to allow keys during system boot
